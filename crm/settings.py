502bbcd07e01d1a8664b08736d386d4260b59bf7
syedanwarulhusain786 <syedanwarulhusain68@gmail.com>
2023-09-20 20:39:18 +0530

    All Names Updated Till Chart of Accouunt

--- crm/settings.py
+++ crm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$dfdzmvj2pg82t^%n7)6q)o5p3gautj0x*gdiiajgt$t-+ez#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['syedanwarulhusain68.pythonanywhere.com','*']
+ALLOWED_HOSTS = ['aarya.group','*']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,13 +123,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[
-        os.path.join(BASE_DIR,'static'),
-        ]
+# "static"
+
+
+
+
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS=[
+        os.path.join(BASE_DIR,'static'),
+        ]
+# STORAGES = {
+#     # ...
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
